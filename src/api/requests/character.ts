03e9da7fe1c228f41efd685564a4e7599c347c3b
unknown <gabriel.paiv.oliv@hotmail.com>
2022-12-10 13:49:55 -0300

    Removing unused modules

--- src/api/requests/character.ts
+++ src/api/requests/character.ts
@@ -1,4 +1,3 @@
-import { AxiosResponse } from "axios";
 import { v4 as uuidv4 } from "uuid";
 import {
   CharacterPaginated,
--- src/app.component.tsx
+++ src/app.component.tsx
@@ -1,24 +1,13 @@
 import { Layout } from "antd";
-import { ReactElement } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import { CharacterDetailsComponent } from "./components/character-details/character-details.component";
 import { CreateCharacterComponent } from "./components/create-character/create-character.component";
 import { HomeComponent } from "./components/home/home.component";
-import { isUserAuthenticated } from "./utils/auth-utils";
 
 import "./app.component.css";
 
 const { Content } = Layout;
-
-// @ts-ignore
-export const ProtectedRoute = ({ children }): ReactElement => {
-  if (!isUserAuthenticated()) {
-    // user is not authenticated
-    return <Navigate to="/signin" />;
-  }
-  return children;
-};
 
 function AppComponent() {
   return (
